b9d233d0462fe6fa7cbed7f90615f66c84e4176f
ilumos <ilumos@gmail.com>
2026-02-26 23:22:31 +0000

    Send Steam IDs as strings to match Steam's API response

--- tests/Datasets/users.php
+++ tests/Datasets/users.php
@@ -1,9 +1,9 @@
 <?php
 
 dataset('userids', function (): iterable {
-    yield 'Gummibeer' => 76561198061912622;
+    yield 'Gummibeer' => '76561198061912622';
 });
 
 dataset('privateuserids', function (): iterable {
-    yield 'Rabscuttle' => 76561197960287930;
+    yield 'Rabscuttle' => '76561197960287930';
 });
